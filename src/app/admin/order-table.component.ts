569dd720cc66ffcd76b56adbb2d4eb525b4118e3
hyrmec <hyrmec@gmail.com>
2018-02-18 20:14:10 +0300

    Admin panel

---
+++ src/app/admin/order-table.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Order } from '../model/order.model';
+import { OrderRepository } from '../model/order.repository';
+
+@Component({
+  selector: 'app-order-table',
+  templateUrl: './order-table.component.html',
+  styleUrls: ['./order-table.component.css']
+})
+export class OrderTableComponent {
+  includeShipped = false;
+
+  constructor(private repository: OrderRepository) {}
+
+  getOrders(): Order[] {
+    return this.repository.getOrders()
+      .filter(o => this.includeShipped || !o.shipped);
+  }
+
+  markShipped(order: Order) {
+    order.shipped = true;
+    this.repository.updateOrder(order);
+  }
+
+  deleted(id: number) {
+    this.repository.deleteOrder(id);
+  }
+
+
+}
